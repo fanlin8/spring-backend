4b195f07b7c7dede6c0b2d47f794db70cc1ddb4a
Erick Lin <erick_lin@ao.uscourts.gov>
2017-08-25 10:46:39 -0400

    updated the sql scripts.

--- src/main/resources/example.sql
+++ src/main/resources/example.sql
@@ -1,3 +1,17 @@
-create database db_example;
+create database if not exists db_example;
 create user 'springuser'@'localhost' identified by 'ThePassword';
 grant all on db_example.* to 'springuser'@'localhost';
+
+USE db_example;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `article_id` int(5) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+INSERT INTO `articles` (`article_id`, `title`, `category`) VALUES
+	(1, 'Java Concurrency', 'Java'),
+	(2, 'Hibernate HQL ', 'Hibernate'),
+	(3, 'Spring MVC with Hibernate', 'Spring'); 
+    
+select * from articles;
